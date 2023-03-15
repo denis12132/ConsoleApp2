1d3c0f87c796c4372e232958d291f2cb5a426000
edu-msk22-809s <edu-msk22-809s@MSK22-EWS0905>
2023-03-15 13:13:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int F, x, y, z;
+        double K;
+        double max = 0;
+        int count = 0;
+
+        for (F = 100; F <= 999; F++)
+        {
+
+            x = F / 100;
+            y = F / 10 % 10;
+            z = F % 10;
+
+            K = F / (x + y + z);
+            if (K < max)
+            {
+                max = K;
+                count = F;
+            }
+            Console.Write(K + " ");
+        }
+        Console.WriteLine("\nНаибольшее значение -> {0}", count);
+
+        Console.ReadLine();
+    }
+}
